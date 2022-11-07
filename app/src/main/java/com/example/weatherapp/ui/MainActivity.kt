14e26ded06ebd69c3b69bd49e3ae762a00786f5f
astha@astha <astha.singh@tothenew>
2022-11-08 00:42:49 +0530

    formatted data getting displayed on main screen.

--- app/src/main/java/com/example/weatherapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/ui/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.weatherapp.ui
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.core.app.ActivityCompat
+import androidx.lifecycle.ViewModelProvider
 import com.example.weatherapp.databinding.ActivityMainBinding
-import com.example.weatherapp.model.WeatherRepository
+import com.example.weatherapp.utils.Resource
+import com.example.weatherapp.utils.TimeUtils
+import com.example.weatherapp.viewmodel.WeatherViewModel
 import com.google.android.gms.location.LocationServices
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -16,7 +20,9 @@ import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mBinding: ActivityMainBinding
-    var weatherRepository: WeatherRepository = WeatherRepository()
+    private val weatherViewModel: WeatherViewModel by lazy {
+        ViewModelProvider(this)[WeatherViewModel::class.java]
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
@@ -45,10 +51,14 @@ class MainActivity : AppCompatActivity() {
                 CoroutineScope(Dispatchers.IO).launch {
 
                     // getWeatherInfoFromRepo("52.52", "13.41")
-                    getWeatherInfoFromRepo(
-                        location?.latitude.toString(),
-                        location?.longitude.toString()
-                    )
+                    TimeUtils.endDate()?.let {
+                        getWeatherInfoFromViewModel(
+                            location?.latitude.toString(),
+                            location?.longitude.toString(),
+                            TimeUtils.today(),
+                            it
+                        )
+                    }
 
                 }
             }
@@ -57,10 +67,28 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    private suspend fun getWeatherInfoFromRepo(latitude: String, longitude: String) {
-        val aa = weatherRepository.getWeatherInfo(latitude, longitude)
+
+    private suspend fun getWeatherInfoFromViewModel(latitude: String, longitude: String, startDate:String, endDate:String) {
+        weatherViewModel.getWeatherInfoFromRepo(latitude, longitude,startDate, endDate)
         CoroutineScope(Dispatchers.Main).launch {
-            mBinding.tvCurrentTemp.text = aa.body()?.currentWeather?.temperature.toString()
+            weatherViewModel.currentWeather.observe(this@MainActivity){response->
+                when(response){
+                    is Resource.Success->{
+                        val currentWeatherDetails=response.data?.currentWeather
+                        mBinding.tvCurrentTemp.text= "Current Temp:${currentWeatherDetails?.temperature.toString()}"
+                        mBinding.tvCurrentTime.text="Time:${currentWeatherDetails?.time.toString()}"
+                        mBinding.tvWindSpeed.text="Windspeed:${currentWeatherDetails?.windspeed.toString()}"
+                    }
+                    is Resource.Error->{
+                        Log.i("Error occured",response.message.toString())
+                    }
+
+                    else -> {
+                        Log.i("Error occured","else")
+                    }
+                }
+
+            }
         }
     }
 
